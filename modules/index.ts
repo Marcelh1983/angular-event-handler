b5a60f3588991487adda4b01422e4163c202a1e8
Marcel Hoekstra <marcel_hoekstra@outlook.com>
2023-06-15 22:43:03 +0200

    updated ng package.json

--- modules/index.ts
+++ modules/index.ts
@@ -1,3 +1,4 @@
 export { EventHandlerDirective as HandleEventDirective } from './event-handler.directive';
+export { OutsideHandlerDirective as OutsideEventDirective } from './outside-handler.directive';
 export { NgxEventHandlerModule } from './event-handler.module';
 export { createObservableHandler } from './event-handler.directive';
